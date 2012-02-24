e0c6237f4d21072d38f53ac502d171805dc2263a
Matthys Kroon <matthysk@gmail.com>
2012-02-24 18:39:21 +0200

    Add TaskStatus to admin register

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin 
-from simpletask.models import Task, Project, Tags
+from simpletask.models import Task, Project, Tags, TaskStatus
 
 admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(Tags)
-
+admin.site.register(TaskStatus)
